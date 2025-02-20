e1df287c4365577a12bd8a261a7adc18b8353844
blott <blottcomms@gmail.com>
2025-02-20 00:51:19 +0000

    Split alo.c

---
+++ src/P2/alo.c
@@ -0,0 +1,425 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "P2/alo", FIsZeroV);
+
+INCLUDE_ASM(const s32, "P2/alo", FIsZeroW);
+
+INCLUDE_ASM(const s32, "P2/alo", FIsZeroDv);
+
+INCLUDE_ASM(const s32, "P2/alo", FIsZeroDw);
+
+INCLUDE_ASM(const s32, "P2/alo", InitAlo__FP3ALO);
+
+INCLUDE_ASM(const s32, "P2/alo", AddAloHierarchy);
+
+INCLUDE_ASM(const s32, "P2/alo", OnAloAdd);
+
+INCLUDE_ASM(const s32, "P2/alo", RemoveAloHierarchy);
+
+INCLUDE_ASM(const s32, "P2/alo", OnAloRemove);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAloOrig);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloParent);
+
+INCLUDE_ASM(const s32, "P2/alo", ApplyAloProxy);
+
+INCLUDE_ASM(const s32, "P2/alo", BindAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", PostAloLoad);
+
+INCLUDE_ASM(const s32, "P2/alo", PostAloLoadCallback);
+
+INCLUDE_ASM(const s32, "P2/alo", SnipAloObjects__FP3ALOiP4SNIP);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAloHierarchy);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAlo__FP3ALOf);
+
+INCLUDE_ASM(const s32, "P2/alo", InvalidateAloLighting);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAloXfWorld);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAloXfWorldHierarchy);
+
+INCLUDE_ASM(const s32, "P2/alo", PresetAloAccel);
+
+INCLUDE_ASM(const s32, "P2/alo", ProjectAloTransform);
+
+INCLUDE_ASM(const s32, "P2/alo", PredictAloTransform);
+
+INCLUDE_ASM(const s32, "P2/alo", PredictAloTransformAdjust);
+
+INCLUDE_ASM(const s32, "P2/alo", DupAloRo);
+
+INCLUDE_ASM(const s32, "P2/alo", RenderFastShadow);
+
+INCLUDE_ASM(const s32, "P2/alo", RenderAloAll);
+
+INCLUDE_ASM(const s32, "P2/alo", RenderAloSelf);
+
+INCLUDE_ASM(const s32, "P2/alo", RenderAloGlobset);
+
+INCLUDE_ASM(const s32, "P2/alo", RenderAloLine);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloOverrideCel);
+
+INCLUDE_ASM(const s32, "P2/alo", func_00126AB0);
+
+INCLUDE_ASM(const s32, "P2/alo", UpdateAloThrob);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloBlotContext);
+
+INCLUDE_ASM(const s32, "P2/alo", EnsureAloFader);
+
+INCLUDE_ASM(const s32, "P2/alo", FadeAloIn__FP3ALOf);
+
+INCLUDE_ASM(const s32, "P2/alo", FadeAloOut__FP3ALOf);
+
+INCLUDE_ASM(const s32, "P2/alo", AdjustAloRtckMat);
+
+INCLUDE_ASM(const s32, "P2/alo", CloneAloHeirarchy);
+
+INCLUDE_ASM(const s32, "P2/alo", CloneAlo__FP3ALOT0);
+
+INCLUDE_ASM(const s32, "P2/alo", HandleAloMessage);
+
+INCLUDE_ASM(const s32, "P2/alo", TranslateAloToPos);
+
+INCLUDE_ASM(const s32, "P2/alo", func_00127468);
+
+INCLUDE_ASM(const s32, "P2/alo", RotateAloToMat);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloVelocityVec);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloVelocityXYZ);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloAngularVelocityVec);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloAngularVelocityXYZ);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloVelocityLocal);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloVelocityLocal);
+
+INCLUDE_ASM(const s32, "P2/alo", MatchAloOtherObject);
+
+INCLUDE_ASM(const s32, "P2/alo", CalculateAloMovement);
+
+INCLUDE_ASM(const s32, "P2/alo", CalculateAloTransform);
+
+INCLUDE_ASM(const s32, "P2/alo", CalculateAloTransformAdjust);
+
+INCLUDE_ASM(const s32, "P2/alo", ConvertAloPos);
+
+INCLUDE_ASM(const s32, "P2/alo", ConvertAloVec);
+
+INCLUDE_ASM(const s32, "P2/alo", ConvertAloMat);
+
+INCLUDE_ASM(const s32, "P2/alo", FDrivenAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", RetractAloDrive);
+
+INCLUDE_ASM(const s32, "P2/alo", ConvertAloMovement);
+
+INCLUDE_ASM(const s32, "P2/alo", CalculateAloDrive);
+
+INCLUDE_ASM(const s32, "P2/alo", FGetAloChildrenList);
+
+INCLUDE_ASM(const s32, "P2/alo", PactsegNewAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", LoadAloFromBrx__FP3ALOP18CBinaryInputStream);
+
+INCLUDE_ASM(const s32, "P2/alo", LoadAloAloxFromBrx);
+
+INCLUDE_ASM(const s32, "P2/alo", BindAloAlox);
+
+INCLUDE_ASM(const s32, "P2/alo", AdjustAloRotation);
+
+INCLUDE_ASM(const s32, "P2/alo", UnadjustAloRotation);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInitialVelocity);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInitialAngularVelocity);
+
+INCLUDE_ASM(const s32, "P2/alo", PasegdEnsureAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloFastShadowRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloFastShadowRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloFastShadowDepth);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloFastShadowDepth);
+
+INCLUDE_ASM(const s32, "P2/alo", PshadowAloEnsure);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloCastShadow);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowShader);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowNearRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowFarRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowNearCast);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowFarCast);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowConeAngle);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloShadowFrustrumUp);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloDynamicShadowObject);
+
+INCLUDE_ASM(const s32, "P2/alo", PshadowInferAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloCastShadow);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowShader);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowNearRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowFarRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowNearCast);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowFarCast);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowConeAngle);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloShadowFrustrumUp);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloEuler);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloEuler);
+
+INCLUDE_ASM(const s32, "P2/alo", EnsureAloActRestore);
+
+INCLUDE_ASM(const s32, "P2/alo", EnsureAloActla);
+
+INCLUDE_ASM(const s32, "P2/alo", RecacheAloActList);
+
+INCLUDE_ASM(const s32, "P2/alo", InsertAloAct);
+
+INCLUDE_ASM(const s32, "P2/alo", ResortAloActList);
+
+INCLUDE_ASM(const s32, "P2/alo", PasegaFindAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", PsmaFindAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", PasegaFindAloNearest);
+
+INCLUDE_ASM(const s32, "P2/alo", CreateAloActadj);
+
+INCLUDE_ASM(const s32, "P2/alo", FIsAloStatic);
+
+INCLUDE_ASM(const s32, "P2/alo", ResolveAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionSpring);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionSpringDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionDamping);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionDampingDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationSpring);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationSpringDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationDamping);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationDampingDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionSmooth);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionSmoothMaxAccel);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPositionSmoothDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationSmooth);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationSmoothMaxAccel);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationSmoothDetail);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloDefaultAckPos);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloDefaultAckRot);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRestorePosition);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a3c8);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloNoFreeze);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a418);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRestorePositionAck);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRestoreRotation);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRestoreRotationAck);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a4e8);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAt);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtIgnore);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtIgnore);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtPanFunction);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtPanFunction);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtPanLimits);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtPanLimits);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtTiltFunction);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtTiltFunction);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtTiltLimits);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtTiltLimits);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtEnabledPriority);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtEnabledPriority);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloLookAtDisabledPriority);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloLookAtDisabledPriority);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a810);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a848);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a860);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a888);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a8b8);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012a8c8);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRotationMatchesVelocity);
+
+INCLUDE_ASM(const s32, "P2/alo", PtargetEnsureAlo);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloTargetAttacks);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloTargetRadius);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloTargetHitTest__FP3ALOi);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloScrollingMasterSpeeds);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloEyesClosed);
+
+INCLUDE_ASM(const s32, "P2/alo", EnsureAloSfx);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloSfxid);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloSfxidSpl);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloSfxid);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloSStart);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloSStart);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloSFull);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloSndRepeat);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloSFull);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloUVolume);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloUDoppler);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloUDoppler);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloUVolumeSpl);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloUVolume);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloUPitch);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloUPitchSpl);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloUPitch);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloSndRepeat);
+
+INCLUDE_ASM(const s32, "P2/alo", StartAloSound);
+
+INCLUDE_ASM(const s32, "P2/alo", StopAloSound);
+
+INCLUDE_ASM(const s32, "P2/alo", EnsureAloThrob);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloThrobKind);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloThrobKind);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloThrobInColor);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloThrobInColor);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloThrobOutColor);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloThrobOutColor);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloThrobDtInOut);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloThrobDtInOut);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractCane);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractCane);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractCaneSweep);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractCaneSweep);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractCaneRush);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractCaneRush);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractCaneSmash);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractCaneSmash);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractBomb);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractBomb);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloInteractShock);
+
+INCLUDE_ASM(const s32, "P2/alo", GetAloInteractShock);
+
+INCLUDE_ASM(const s32, "P2/alo", FAbsorbAloWkr);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloPoseCombo);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloForceCameraFade);
+
+INCLUDE_ASM(const s32, "P2/alo", SetAloRealClock);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b550);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b590);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b5b8);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012B6A0);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b6b8);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b760);
+
+INCLUDE_ASM(const s32, "P2/alo", func_0012b868);
